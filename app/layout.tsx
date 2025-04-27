096dd6fbb2ec3f31d10482999cbe953cab6eec05
Ravi Patel <ravifromchanga@gmail.com>
2025-04-27 15:37:56 +0530

    fav icon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,24 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import Header from "@/components/header"
-import Footer from "@/components/footer"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CILASS - Charotar Institute of Languages, Arts and Social Studies",
-  description: "Charotar Institute of Languages, Arts and Social Studies - Faculty of Humanities at CHARUSAT",
-    generator: 'v0.dev'
-}
+  title: "CLASS - Charotar Institute of Languages, Arts and Social Studies",
+  description:
+    "Charotar Institute of Languages, Arts and Social Studies - Faculty of Humanities at CHARUSAT",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -31,5 +32,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
